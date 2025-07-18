f057cb5b444c992442d0850b2f32068080e75790
Moon Hub <3977373683@qq.com>
2025-07-18 13:06:33 +0800

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,4 @@
+local Gamelist = {
+  ["Elemental Powers Tcyoon"] = "https://raw.githubusercontent.com/Moxiaobai-Shine-Star/Roblox/refs/heads/main/Server/Elemental%20Powers%20Tcyoon.lua",
+  ["Aooni Nightmare"] = "https://raw.githubusercontent.com/Moxiaobai-Shine-Star/Roblox/refs/heads/main/Server/Aooni%20Nightmare.lua"
+}
